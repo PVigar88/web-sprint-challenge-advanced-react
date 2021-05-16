523d343c6eef33ba60fa145feb11efbe6e0baa90
PVigar88 <Pvigar@gmail.com>
2021-05-16 18:28:44 -0400

    finished MVP and testing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,65 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
+  const formHeader = screen.getByText("Checkout Form");
+  expect(formHeader).toBeDefined();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm />);
+  //test input data
+  const firstName = "John";
+  const lastName = "Smith";
+  const address = "123 sesame street";
+  const city = "NoWhere";
+  const state = "WI";
+  const zip = "45678";
+  //selecting inputs
+  const firstNameInput = screen.getByLabelText(/First Name/i);
+
+  const lastNameInput = screen.getByLabelText(/Last Name/i);
+
+  const addressInput = screen.getByLabelText(/Address/i);
+
+  const cityInput = screen.getByLabelText(/City/i);
+
+  const stateInput = screen.getByLabelText(/State/i);
+
+  const zipInput = screen.getByLabelText(/Zip/i);
+
+  const checkOutButton = screen.getByRole("button");
+  //inputing data via userEvent
+  userEvent.type(firstNameInput, firstName);
+
+  userEvent.type(lastNameInput, lastName);
+
+  userEvent.type(addressInput, address);
+
+  userEvent.type(cityInput, city);
+
+  userEvent.type(stateInput, state);
+
+  userEvent.type(zipInput, zip);
+
+  userEvent.click(checkOutButton);
+  //validation
+  const successDisplay = screen.getByTestId("successMessage");
+
+  expect(successDisplay).toHaveTextContent(firstName);
+
+  expect(successDisplay).toHaveTextContent(lastName);
+
+  expect(successDisplay).toHaveTextContent(address);
+
+  expect(successDisplay).toHaveTextContent(city);
+
+  expect(successDisplay).toHaveTextContent(state);
+
+  expect(successDisplay).toHaveTextContent(zip);
+});
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,18 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+export const useForm = (key, initialValues) => {
+  const [state, setState, cb] = useState(key, initialValues);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const handleChanges = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+
+  return [state, handleChanges, handleSubmit, showSuccessMessage];
+};
